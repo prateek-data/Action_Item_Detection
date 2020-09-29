58d5785a6978d1cfaca35429abe3761a29c922a9
prateek-data <prateek4015@gmail.com>
2020-09-30 02:21:19 +0530

    Update code pipeline

--- run_pipeline.py
+++ run_pipeline.py
@@ -10,8 +10,8 @@ import pandas as pd
 
 from data_preprocessing import process_enron_sample
 from nlp_rule_match import spacy_check_email_list
-from ml_classifier import data_loader, get_features, data_split, classify_logistic, classify_random_forest
-from dl_models import data_processing, load_embeddings, train_lstm, save__model, load__model, evaluate_model
+from ml_classifier import data_loader, get_features, data_split, classify_logistic, classify_random_forest, save_ml_model
+from dl_models import data_processing, load_embeddings, train_lstm, save_dl_model, load_dl_model, evaluate_model
 
 
 
@@ -20,12 +20,12 @@ actionCsv = "Datasets/actions.csv"
 
 #data preparation
 ##work on a sample of 5k emails from enron dataset due to memory constraints
-sentenceList = process_enron_sample(inputCsv, sampleSize = 3000)
+sentenceList = process_enron_sample(inputCsv, sampleSize = 5000)
 
 #run nlp matcher on sentenceList to classify actionable and non actionable sentences
 tempList = sentenceList[0:3000]
 nlpResultList =  spacy_check_email_list(tempList)
-
+del sentenceList
 
 #Using ML Models
 ##Load data
@@ -53,15 +53,21 @@ textList, gtList = data_loader(allText)
 featureArray, gtArray = get_features(textList, gtList)
 trainX, testX, trainY, testY = data_split(featureArray, gtArray)
 
+
 #train classifiers and print accuracy on test set
 ##logistic regression model
-logClf, logMetrics = classify_logistic(trainX, testX, trainY, testY)
-print("Accuracy from logistic classifier : ", logMetrics)
+logClf, logMetrics = classify_logistic(trainX, trainY, featureArray, gtArray)
+print("Performance metrics from logistic classifier : ", logMetrics)
+#save model
+save_ml_model(logClf, "Logistic_Regression_Classifier")
+del logClf
 
 ##random forest model
-rfClf, rfMetrics = classify_random_forest(trainX, testX, trainY, testY)
-print("Accuracy from random forest classifier : ", rfMetrics)
-
+rfClf, rfMetrics = classify_random_forest(trainX, trainY, featureArray, gtArray)
+print("Performance metrics from random forest classifier : ", rfMetrics)
+#save model
+save_ml_model(rfClf, "Random_Forest_Classifier")
+del rfClf
 
 #######################################################################################################
 print("###############################################################################################")
@@ -79,14 +85,15 @@ trainX, testX, trainY, testY = data_split(featureArray, gtArray)
 ''' 
 embeddingMatrix = load_embeddings(tokenizer, vocabSize)
 lstmModel = train_lstm(trainX, trainY, maxLen, vocabSize, embeddingMatrix)
-save__model(lstmModel, "Saved_Models/Lstm_Model")
+save_dl_model(lstmModel, "Saved_Models/Lstm_Model")
 '''
 
 #load LSTM model
-lstmModel = load__model("Saved_Models/Lstm_Model")
+lstmModel = load_dl_model("Saved_Models/Lstm_Model")
 #evaluate LSTM model
 lstmMetrics = evaluate_model(lstmModel, testX, testY)
-print(lstmMetrics)
+print("Performance metrics from LSTM Model : ", lstmMetrics)
+del lstmModel
 
 
 
@@ -95,14 +102,15 @@ print(lstmMetrics)
 '''
 embeddingMatrix = load_embeddings(tokenizer, vocabSize)
 cnnModel = train_cnn(trainX, trainY, maxLen, vocabSize, embeddingMatrix)
-save__model(cnnModel, "Saved_Models/Cnn_Model")
+save_dl_model(cnnModel, "Saved_Models/Cnn_Model")
 '''
 
 #Load CNN model
-cnnModel = load__model("Saved_Models/Cnn_Model")
+cnnModel = load_dl_model("Saved_Models/Cnn_Model")
 #evaluate CNN model
 cnnMetrics = evaluate_model(cnnModel, testX, testY)
-print(cnnMetrics)
+print("Performance metrics from CNN Model : ", cnnMetrics)
+del cnnModel
 
 
 #######################################################################################################
