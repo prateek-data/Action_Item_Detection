52df6d90dc945be185c46574fb2cbd0113421a1b
prateek-data <prateek4015@gmail.com>
2020-09-29 18:12:50 +0530

    Updated Flask API Code

--- server.py
+++ server.py
@@ -9,7 +9,9 @@ Created on Fri Sep 25 13:42:12 2020
 import flask
 from flask import request
 
-from nlp_rule_match import check_sentence
+from nlp_rule_match import spacy_check_sentence
+from ml_classifier import load_ml_model, run_ml_inference
+from dl_models import load_dl_model, run_dl_inference
 
 app = flask.Flask(__name__)
 app.config["DEBUG"] = True
@@ -21,24 +23,67 @@ def hello_world():
     return 'Hello, World!'
 
 @app.route('/nlp_matcher', methods=['POST'])
-def get_input_request():
-    
+def nlp_matcher():
+    #api for nlp rule based matcher
     assert request.path == '/nlp_matcher'
     assert request.method == 'POST'
     
+    
     data = request.json    
     sentence = data['Text']
-    result = check_sentence(sentence)
+    
+    result = spacy_check_sentence(sentence)
     
     if result != None:
         resDict = {}
         resDict['Text'] = sentence
         resDict['Is_Actionable'] = result
+        return (resDict)
+   
     
+@app.route('/ml_classifier', methods=['POST'])
+def ml_classifier():
+    #api for ml classifier 
+    assert request.path == '/ml_classifier'
+    assert request.method == 'POST'
+    
+    data = request.json    
+    sentence = data['Text']
+    
+    #load model
+    rfClf =  load_ml_model("Random_Forest_Classifier")
+    result = run_ml_inference(rfClf, sentence)
+    del rfClf
+    
+    if result != None:
+        resDict = {}
+        resDict['Text'] = sentence
+        resDict['Is_Actionable'] = result
+        return (resDict)
+ 
+    
+@app.route('/dl_model', methods=['POST'])
+def dl_model():
+    #api for dl model 
+    assert request.path == '/dl_model'
+    assert request.method == 'POST'
+    
+    data = request.json    
+    sentence = data['Text']
+    
+    #load model
+    lstmModel =  load_dl_model("Saved_Models/Lstm_Model")
+    result = run_dl_inference(lstmModel, sentence)
+    del lstmModel
+    
+    if result != None:
+        resDict = {}
+        resDict['Text'] = sentence
+        resDict['Is_Actionable'] = result
         return (resDict)
     
-    
-    
+   
+
 if __name__ == '__main__':
     app.run(debug=True , port= 5000) 
     
