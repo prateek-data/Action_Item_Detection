06f4c9b53fa7720da2713c8f4fff2448f3bedf4a
prateek-data <prateek4015@gmail.com>
2020-09-30 22:31:06 +0530

    Updated download script

--- download_script.py
+++ download_script.py
@@ -5,49 +5,20 @@ Created on Tue Sep 29 14:35:21 2020
 
 @author: user
 """
-
+# Code to download datasets and additional dependencies.
+# Will take some time to execute based of internet speed.
 import os
-import urllib
-import progressbar
 import nltk
 import spacy
 
 
-def show_progress(block_num, block_size, total_size):
-    #function to show download progress
-    global pbar
-    if pbar is None:
-        pbar = progressbar.ProgressBar(maxval=total_size)
-        pbar.start()
-
-    downloaded = block_num * block_size
-    if downloaded < total_size:
-        pbar.update(downloaded)
-    else:
-        pbar.finish()
-        pbar = None
-
-
-
 #download spacy md model
 spacy.cli.download("en_core_web_md")
+print("Downloaded Spacy en_core_web_md model.") 
+
 
 #download nltk dependencies
 nltk.download('punkt')
 nltk.download('averaged_perceptron_tagger')
+print("Downloaded NLTK dependencies.")
 
-
-#download Enron dataset from Kaggle 
-pbar = None
-url = "https://www.kaggle.com/wcukierski/enron-email-dataset"
-fileDest = "temp_data"
-
-urllib.request.urlretrieve(url, os.path.join(fileDest, 'archive.zip'), show_progress)
-
-
-#download Glove vectors
-pbar = None
-url = "http://nlp.stanford.edu/data/glove.6B.zip"
-fileDest = "temp_data"
-
-urllib.request.urlretrieve(url, os.path.join(fileDest, 'glove.6B.zip'), show_progress)
